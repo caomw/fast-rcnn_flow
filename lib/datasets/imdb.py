6b0c2a9b52a11a0f765f850eb0ca2755edc8bc43
xiaolonw <xiaolonw@cs.cmu.edu>
2015-11-24 20:45:20 -0500

    roidb

--- lib/datasets/imdb.py
+++ lib/datasets/imdb.py
@@ -101,6 +101,9 @@ class imdb(object):
             oldx2 = boxes[:, 2].copy()
             boxes[:, 0] = widths[i] - oldx2 - 1
             boxes[:, 2] = widths[i] - oldx1 - 1
+            if (boxes[:, 2] >= boxes[:, 0]).all() == False:
+                print boxes
+                print self.image_path_at(i)
             assert (boxes[:, 2] >= boxes[:, 0]).all()
             entry = {'boxes' : boxes,
                      'gt_overlaps' : self.roidb[i]['gt_overlaps'],
