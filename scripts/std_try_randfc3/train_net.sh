022530435638ce0f13478a2886f3536238ca4096
xiaolonw <xiaolonw@cs.cmu.edu>
2015-11-26 00:57:17 -0500

    iterations

--- scripts/std_try_randfc3/train_net.sh
+++ scripts/std_try_randfc3/train_net.sh
@@ -1,6 +1,8 @@
 
 #!/usr/bin/env sh
 
-../../tools/train_net.py --gpu 1 --solver solver.prototxt  --weights /nfs.yoda/xiaolonw/vol1_video_models/ucf_vgg16_flow_cls/video__iter_30000.caffemodel   
+../../tools/train_net.py --gpu 0 --solver solver.prototxt  --weights /nfs.yoda/xiaolonw/imagenet/data/models/std_try_fcrand3/bef_stop/vgg16_fast_rcnn_iter_24000.caffemodel
+
+# /nfs.yoda/xiaolonw/vol1_video_models/ucf_vgg16_flow_cls/video__iter_30000.caffemodel   
 
 
